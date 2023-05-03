24a8851c763f58305655a182fe645a1eb38ba46c
A-amirul <amirulislamamir412@gmail.com>
2023-05-03 13:21:55 +0600

    get specific chef data for specific id

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 const chefs = require('./data/chefs.json');
 
+
 app.get('/', (req, res) => {
 	res.send("chef recipe is running")
 });
 
 app.get('/chefs', (req, res) => {
 	res.send(chefs);
+})
+
+
+app.get('/chefs/:id', (req, res) => {
+	const id = req.params.id;
+	const selectedChefs =chefs.find(s => s.id == id)
+	res.send(selectedChefs);
 })
 
 app.listen(port, () => {
